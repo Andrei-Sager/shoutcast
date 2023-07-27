0ce909b2415a828f9fc2aae488a833308feeaabc
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-27 15:02:41 +0800

    Update HomeFeed.jsx

--- src/components/HomeFeed.jsx
+++ src/components/HomeFeed.jsx
@@ -38,6 +38,33 @@ const HomeFeed = () => {
           @ Copyright 2023 Andrei Sager
         </Typography>
       </Box>
+
+      <Box
+        p={2}
+        sx={{
+          overflowY: 'auto',
+          height: '92vh',
+          flex: 2,
+        }}
+      >
+        <Typography 
+          variant='h4'
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: 'white'
+          }}
+        >
+          New 
+          <span 
+            style={{ 
+              color: '#FC3150'
+            }}
+          >
+            videos
+          </span>
+        </Typography>
+      </Box>
     </Stack>
   )
 }
