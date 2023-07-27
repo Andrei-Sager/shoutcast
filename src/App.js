9c67839fed1f8f5a619bc1d080af030e7eb1210c
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-27 09:43:00 +0800

    Created components
    +Five components
    +An export file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
-import React from 'react'
+
+import { Navbar, HomeFeed, SearchFeed, VideoDetails, ChannelDetails} from './components'
 
 const App = () => (
     <BrowserRouter>
         <Box sx={{backgroundColor: '#000'}}>
             <Navbar />
             <Routes>
-                <Route path='/' exact element={<Feed />} />
+                <Route path='/' exact element={<HomeFeed />} />
                 <Route path='/video/:id' element={<VideoDetails />} />
                 <Route path='/channel/:id' element={<ChannelDetails />} />
                 <Route path='/search/:searchTerm' element={<SearchFeed />} />
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar'
+export { default as HomeFeed } from './HomeFeed'
+export { default as SearchFeed } from './SearchFeed'
+export { default as VideoDetails } from './VideoDetails'
+export { default as ChannelDetails } from './ChannelDetails'
