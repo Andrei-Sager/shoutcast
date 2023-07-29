5df11c7c1e0f2262457d6c1f56d9d1f8dc15fba1
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-29 09:56:26 +0800

    Update ChannelDetails.jsx

--- src/components/ChannelDetails.jsx
+++ src/components/ChannelDetails.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/material'
 
-import { Video, ChannelCard } from './'
+import { Video, ChannelCard, Videos } from './'
 import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const ChannelDetails = () => {
@@ -17,8 +17,26 @@ const ChannelDetails = () => {
   }, [id])
 
   return (
-    <Box>
-      {id}
+    <Box
+      minHeight="95vh"
+    >
+      <Box>
+        <div style={{
+            background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+            zIndex: 10,
+            height: '300px'
+        }}
+      />
+        <ChannelCard channelDetails={channelDetails} marginTop={'-120px'}></ChannelCard>
+      </Box>
+      <Box display='flex' p="2"> 
+        <Box 
+          sx={{
+            mr: { sm:'100px'}
+          }}
+        />
+          <Videos videos={videos}/>
+      </Box>
     </Box>
   )
 }
